fab00ab6f12d8c552a18edf3586fa7472a21d0b3
Damola Olusakin <olusakinadedamola@gmail.com>
2023-07-03 10:32:41 -0400

    setup starter file for mongoose-flight proj

--- models/flight.js
+++ models/flight.js
@@ -1,2 +1,17 @@
-const mongoose = require('mongose')
+const mongoose = require("mongoose");
 
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+  airline: {
+    type: String,
+  },
+  aiport: { type: String },
+  airline: { 
+    type: Number,
+    min: 10,
+    max: 999,
+    required: true,
+  },
+  depature: {},
+});
