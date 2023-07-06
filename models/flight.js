d50df72146759c1cddc5d75f676b39430eda3ae6
Damola Olusakin <olusakinadedamola@gmail.com>
2023-07-06 13:35:26 -0400

    added enum for airport and airline

--- models/flight.js
+++ models/flight.js
@@ -2,22 +2,40 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const flightSchema = new Schema({
-  airline: {
+const destinationSchema = new Schema({
+  airport: {
     type: String,
+    enum: ["AUS", "DFW", "DEN", "LAX", "SAN"],
   },
-  airport: { type: String },
-  flightNumber: { 
-    type: Number,
-    min: 10,
-    max: 999,
-    required: true,
+  arrival: {
+    type: Date,
   },
-  depature: {
-    
-  },
-}, {
-  timestamps: true
 });
 
-module.exports = mongoose.model('Flight', flightSchema); 
+const flightSchema = new Schema(
+  {
+    airline: {
+      type: String,
+      enum: ['American', 'Southwest', 'United']
+    },
+    airport: {
+      type: String,
+      enum: ["AUS", "DFW", "DEN", "LAX", "SAN"],
+    },
+    flightNumber: {
+      type: Number,
+      min: 10,
+      max: 999,
+      required: true,
+    },
+    depature: {
+      type: Date,
+    },
+    destinations: [destinationSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Flight", flightSchema);
