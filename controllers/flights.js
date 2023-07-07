563fd1bdc2a5f68eb1be82e27ce53d1048e470b0
Damola Olusakin <olusakinadedamola@gmail.com>
2023-07-07 09:31:27 -0400

    created new ticket view

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,39 +1,42 @@
-const FlightModel = require('../models/flight')
-
+const FlightModel = require("../models/flight");
+const TicketModel = require("../models/ticket");
 
 module.exports = {
- index,
- new: newFlight,
- create,
- show,
+  index,
+  new: newFlight,
+  create,
+  show,
+};
+
+async function index(req, res) {
+  const flights = await FlightModel.find({});
+  res.render("flights/index", { title: "All Flights", flights: flights });
 }
 
-async function index(req, res){
-  const flights = await FlightModel.find({})
-  res.render('flights/index', {title: 'All Flights', flights: flights})
+function newFlight(req, res) {
+  res.render("flights/new");
 }
 
-function newFlight(req,res){
-  res.render('flights/new')
+async function show(req, res) {
+  await FlightModel.findById(req.params.id).then((flight) => {
+    TicketModel.find({ flight: flight._id }).then((ticket) => {
+      console.log(ticket);
+      res.render("flights/show", { flight, ticket });
+    });
+  });
 }
 
-async function show(req, res){
-  const flightsFromDatabase = await FlightModel.findById(req.params.id)
-  res.render('flights/show', {flight: flightsFromDatabase})
+async function create(req, res) {
+  console.log(req.body);
+  try {
+    const flightFromTheDatabase = await FlightModel.create(req.body);
 
-}
-
-async function create(req,res){
-  console.log(req.body)
-  try{
-    const flightFromTheDatabase = await FlightModel.create(req.body)
-
-    console.log(flightFromTheDatabase)
-    res.redirect('/flights')
-  }catch(e){
-    console.log(e)
-    res.render('movies/new', {
-      errorMsg: e.message
-    })
+    console.log(flightFromTheDatabase);
+    res.redirect("/flights");
+  } catch (e) {
+    console.log(e);
+    res.render("movies/new", {
+      errorMsg: e.message,
+    });
   }
 }
--- routes/flights.js
+++ routes/flights.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const flightsCtrl = require("../controllers/flights");
+const ticketsCtrl = require("../controllers/tickets");
 
 //Get /flights
 router.get("/", flightsCtrl.index);
 //GET /flights/new
-router.get('/new', flightsCtrl.new);
+router.get("/new", flightsCtrl.new);
 //POST /flights
-router.post('/', flightsCtrl.create);
+router.post("/", flightsCtrl.create);
 
-router.get('/:id', flightsCtrl.show)
+router.get("/:id", flightsCtrl.show);
+
+router.get("/:id/tickets/new", ticketsCtrl.new);
 
 module.exports = router;
